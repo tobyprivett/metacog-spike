3c89e6d89e33c1de54a281b8227a269b6e5b1c31
Toby Privett <toby@snaplab.co.uk>
2015-10-14 13:47:18 +0100

    Initial metacog setup
    - /scripts/config.js is ignore by git but 
    required locally, see config.js.example.

---
+++ scripts/instrumentation.js
@@ -0,0 +1,11 @@
+Metacog.init({
+    "session": {
+        "publisher_id": PUBLISHER_ID,
+        "application_id": APPLICATION_ID,
+        "widget_id": 'test-widget-01',
+        "learner_id": "test-learner-01",
+        "session_id": "test-session-01"
+    },
+    log_tab: true,
+    mode:"production"
+});
