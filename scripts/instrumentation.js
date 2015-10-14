2d72f473135ccda53dd0505fa9ba0605f604ff45
Toby Privett <toby@snaplab.co.uk>
2015-10-14 14:15:05 +0100

    Enable the Metacog training toolbar

--- scripts/instrumentation.js
+++ scripts/instrumentation.js
@@ -2,10 +2,10 @@ Metacog.init({
     "session": {
         "publisher_id": PUBLISHER_ID,
         "application_id": APPLICATION_ID,
-        "widget_id": 'test-widget-01',
+        "widget_id": "test-widget-01",
         "learner_id": "test-learner-01",
         "session_id": "test-session-01"
     },
     log_tab: true,
-    mode:"production"
+    mode:"training"
 });
